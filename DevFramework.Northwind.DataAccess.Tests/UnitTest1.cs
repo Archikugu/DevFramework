42bbcfc2440241a03e4cf75ca5633e5d0d6c35c5
Gokhan <engokhangok@gmail.com>
2024-04-09 22:37:34 +0300

    Add project files.

---
+++ DevFramework.Northwind.DataAccess.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DevFramework.Northwind.DataAccess.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
