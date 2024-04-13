cd6c42c1eca138a69423985678ab39863d3a3394
Gokhan <engokhangok@gmail.com>
2024-04-13 12:08:09 +0300

    Added Entity Framework Implementation

---
+++ DevFramework.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,34 @@
+﻿using DevFramework.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _context;
+        private DbSet<T> _entities;
+
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<T> Table => this.Entities;
+        protected virtual DbSet<T> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                {
+                    _entities = _context.Set<T>();
+                }
+                return _entities;
+            }
+        }
+        // protected virtual DbSet<T> Entities => _entities ?? (_entities = _context.Set<T>());
+    }
+}
