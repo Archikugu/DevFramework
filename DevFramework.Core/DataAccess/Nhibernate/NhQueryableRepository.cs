27acba21832a77ad5f916e7906ee7d3a5c90f6d1
Gokhan <engokhangok@gmail.com>
2024-04-13 14:02:03 +0300

    Added NHibernate Implementation

---
+++ DevFramework.Core/DataAccess/Nhibernate/NhQueryableRepository.cs
@@ -0,0 +1,36 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.Nhibernate
+{
+    public class NhQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private NhibernateHelper _nhibernateHelper;
+        IQueryable<T> _entities;
+
+        public NhQueryableRepository(NhibernateHelper nhibernateHelper)
+        {
+            _nhibernateHelper = nhibernateHelper;
+        }
+
+        public IQueryable<T> Table
+        {
+            get { return this.Entities; }
+        }
+        public virtual IQueryable<T> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                {
+                    _entities = _nhibernateHelper.OpenSession().Query<T>();
+                }
+                return _entities;
+            }
+        }
+    }
+}
