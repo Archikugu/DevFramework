169f75b44f6b757d0133f58640e618d84c7e0a6a
Gokhan <engokhangok@gmail.com>
2024-04-15 11:50:53 +0300

    A complex type was added for the product, and the tables were joined for both EF and NHibernate.

--- DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DevFramework.Core.DataAccess.Nhibernate;
 using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,26 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NhProductDal : NhEntityRepositoryBase<Product>, IProductDal
     {
+        private NhibernateHelper _nHibernateHelper;
         public NhProductDal(NhibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
         }
+
+        public List<ProductDetail> GetProductDetail()
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                             join c in session.Query<Category>() on p.CategoryId equals c.CategoryId
+                             select new ProductDetail
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                             };
+                return result.ToList();
+            }
+        }
     }
+}
 }
