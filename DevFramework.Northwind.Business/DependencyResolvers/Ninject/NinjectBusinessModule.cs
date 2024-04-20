e128e1eec7ed32c6dbeecca3f39ed9bc30d1cd86
Gokhan <engokhangok@gmail.com>
2024-04-20 19:41:44 +0300

    The Ninject library for dependency injection was added and the module was written

---
+++ DevFramework.Northwind.Business/DependencyResolvers/Ninject/NinjectBusinessModule.cs
@@ -0,0 +1,31 @@
+﻿using DevFramework.Core.DataAccess;
+using DevFramework.Core.DataAccess.EntityFramework;
+using DevFramework.Core.DataAccess.Nhibernate;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.Concrete.Managers;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.DependencyResolvers.Ninject
+{
+    public class NinjectBusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+
+            Bind(typeof(IQueryableRepository<>)).To(typeof(EfQueryableRepository<>));
+            Bind<DbContext>().To<NorthwindContext>();
+            Bind<NhibernateHelper>().To<SqlServerHelper>();
+        }
+    }
+}
---
+++ DevFramework.Northwind.Business/DependencyResolvers/Ninject/NinjectValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
+using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.DependencyResolvers.Ninject
+{
+    public class NinjectValidationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Product>>().To<ProductValidator>().InSingletonScope();
+        }
+    }
+}
